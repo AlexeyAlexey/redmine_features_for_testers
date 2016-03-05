2dd4a0e5c11426d38adc50744a4d1c4724ddaaa0
AlexeyAlexey <alexey.kondratenko@mail.ru>
2016-03-05 20:51:25 +0200

    add test

--- test/functional/switch_admin_to_user_and_back_controller_test.rb
+++ test/functional/switch_admin_to_user_and_back_controller_test.rb
@@ -1,8 +1,76 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class SwitchAdminToUserAndBackControllerTest < ActionController::TestCase
+  fixtures :projects,
+           :users, :email_addresses,
+           :roles,
+           :members,
+           :member_roles,
+           :issues,
+           :issue_statuses,
+           :issue_relations,
+           :versions,
+           :trackers,
+           :projects_trackers,
+           :issue_categories,
+           :enabled_modules,
+           :enumerations,
+           :attachments,
+           :workflows,
+           :custom_fields,
+           :custom_values,
+           :custom_fields_projects,
+           :custom_fields_trackers,
+           :time_entries,
+           :journals,
+           :journal_details,
+           :queries,
+           :repositories,
+           :changesets
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  include Redmine::I18n
+
+  def setup
+    User.current = nil
+    @request.env["HTTP_REFERER"] = "http://0.0.0.0:3000/"
+  end
+
+  def test_switch_admin_to_user
+  	Role.find(1).add_permission! :switch_admin_to_user_and_back
+  	#Role.find(1).remove_permission! :add_project
+  	user = User.find_by_id 2
+  	user.admin = true
+
+  	user.save!
+    @request.session[:user_id] = 2
+    post :swith_admine_user
+
+    assert_not User.find_by_id(2).admin?, "must switch admin to user"
+  end
+
+  def test_switch_user_to_admin
+  	Role.find(1).add_permission! :switch_admin_to_user_and_back
+    #Role.find(1).remove_permission! :add_project
+    user = User.find_by_id 2
+    user.admin = false
+    user.save!
+    @request.session[:user_id] = 2
+
+    post :swith_admine_user
+
+    assert user.reload.admin?, "must switch user to admin"
+  end
+
+  def test_must_not_switch_user_to_admin
+  	#Role.find(1).add_permission! :switch_admin_to_user_and_back
+    Role.find(1).remove_permission! :switch_admin_to_user_and_back
+    user = User.find_by_id 2
+    user.admin = false
+    user.save!
+    @request.session[:user_id] = 2
+
+    post :swith_admine_user
+
+    assert_not user.reload.admin?, "must not switch user to admin"
   end
 end
